d7c2c2af93db795959a48d651f7a617474945319
Ali-Amir Aldan <aliamir@mit.edu>
2016-05-02 01:22:22 -0400

    Added files for the final project demo

---
+++ server.py
@@ -0,0 +1,90 @@
+import pika
+from collections import defaultdict
+import time
+
+R = [defaultdict(float), defaultdict(float)]
+LAPTOP_DISTANCE = 5.0
+PIXEL_DISTANCE = 200
+WIDTH = 1024
+HEIGHT = 800
+
+X = defaultdict(float)
+Y = defaultdict(float)
+
+alpha = 0.9
+
+def rolling_update(dic, key, new_val, alpha):
+  if not key in dic:
+    dic[key] = new_val
+  else:
+    dic[key] = dic[key] * (1.0 - alpha) + new_val * alpha
+
+def update(rid, mac, dis):
+  rolling_update(R[rid], mac, dis, alpha)
+
+######################
+###### GEOMETRY ######
+######################
+def intersect(r0, r1):
+  add = max(0.0, (LAPTOP_DISTANCE - r0 - r1)/2.0)
+  r0 += add
+  r1 += add
+
+  dx = (max(0.0, r0**2.0 - ((r1**2.0 - r0**2.0 - LAPTOP_DISTANCE**2.0)**2.0) / ((2*LAPTOP_DISTANCE)**2.0)))**0.5
+
+  dy = LAPTOP_DISTANCE/2.0 - ((r1**2.0 - dx**2.0)**0.5)
+  return dx, dy
+
+######################
+######## UI ##########
+######################
+from Tkinter import *
+root = Tk()
+def drawcircle(canv,x,y,rad):
+    canv.create_oval(x-rad,y-rad,x+rad,y+rad,width=0,fill='blue')
+
+canvas = Canvas(width=WIDTH, height=HEIGHT, bg='white')
+canvas.pack(expand=YES, fill=BOTH)
+def ui_update():
+  print "entered"
+  y0 = HEIGHT / 2 - PIXEL_DISTANCE / 2
+  x0 = 40
+  y1 = HEIGHT / 2 + PIXEL_DISTANCE / 2
+  x1 = 40
+
+  canvas.delete("all")
+  circ1=drawcircle(canvas,x0, y0, 20)
+  circ2=drawcircle(canvas,x1, y1, 20)
+  for mac in R[0]:
+    if not mac in R[1]:
+      continue
+    x, y = intersect(R[0][mac], R[1][mac])
+    print x,y
+    x_cen = int(x * PIXEL_DISTANCE / LAPTOP_DISTANCE + 40)
+    y_cen = int(y * PIXEL_DISTANCE / LAPTOP_DISTANCE + HEIGHT / 2)
+    drawcircle(canvas, x_cen, y_cen, 30)
+
+  text = canvas.create_text(50,10, text="tk test")
+  root.update()
+  print "exited"
+
+######################
+######## RABBIT_MQ ##########
+######################
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='6.857', durable=True)
+
+def callback(ch, method, properties, body):
+    print " [x] Received %r" % (body,)
+    rid, mac, dis = body.split('|')
+    rid = int(rid)
+    dis = float(dis)
+    update(rid, mac, dis)
+    ui_update()
+
+channel.basic_consume(callback,
+                      queue="6.857",
+                      no_ack=True)
+channel.start_consuming()
---
+++ sniffer.cpp
@@ -0,0 +1,70 @@
+#include <SimpleAmqpClient/BasicMessage.h>
+#include <SimpleAmqpClient/Channel.h>
+#include <SimpleAmqpClient/Util.h>
+
+#include <iostream>
+#include <cstring>
+#include <cmath>
+#include <tins/tins.h>
+
+using namespace Tins;
+
+AmqpClient::Channel::ptr_t channel;
+AmqpClient::BasicMessage::ptr_t msg_in;
+
+double SignalToDistanceMeters(int dbm, int freq) {
+  double exp = (27.55 - (20 * log(freq)/log(10.0)) + fabs(dbm*1.0)) / 20.0;
+  return pow(10.0, exp); 
+}
+
+bool callback(const PDU &pdu) {
+  try {
+    const RadioTap *rad = pdu.find_pdu<RadioTap>();
+    if (!rad) {
+      return true;
+    }
+    int strength = (int)rad->dbm_signal();
+    int freq = (int)rad->channel_freq();
+
+    const Dot11Data *dot = pdu.find_pdu<Dot11Data>();
+    if (!dot) {
+      return true;
+    }
+    std::string src = dot->src_addr().to_string();
+    std::string tar = dot->dst_addr().to_string();
+    std::string target_device = "38:71:de:4c:84:2d";
+    //std::string target_device = "d0:22:be:65:f4:a9";
+    if (src == target_device) {
+      double distance = SignalToDistanceMeters(strength, freq);
+      msg_in->Body("1|" + target_device + "|" + std::to_string(distance));
+      channel->BasicPublish("amq.direct", "key", msg_in);
+      msg_in->Body("0|" + target_device + "|" + std::to_string(distance));
+      channel->BasicPublish("amq.direct", "key", msg_in);
+      std::cout << src << " -> " << tar
+                << " signal: " << strength
+                << " dbm. Estimated distance: "
+                << distance << " m." << std::endl;
+    }
+  } catch(const field_not_present &e) {
+    // Nothing 
+  } catch(const std::exception &e) {
+    std::cerr << "Exception " << e.what() << std::endl;
+  }
+
+  return true;
+}
+
+int main() {
+  std::string broker_address = "";
+  channel = AmqpClient::Channel::Create(broker_address);
+  channel->DeclareQueue("6.857", true);
+  channel->BindQueue("6.857", "amq.direct", "key");
+  msg_in = AmqpClient::BasicMessage::Create();
+
+  SnifferConfiguration config;
+  config.set_promisc_mode(true);
+  config.set_rfmon(true);
+  config.set_buffer_size(80 << 20);
+  //config.set_filter("ether host d0:22:be:65:f4:a9");
+  Sniffer("en0", config).sniff_loop(callback);
+}
